3b6ed92babd6e1d27513b60ebe11f417cc78b9ee
jishnu anil <jishnuanil255@gmail.com>
2024-11-21 10:47:49 +0530

    fixed render issue and updated instance server

--- src/configs/axiosInstance.js
+++ src/configs/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://fts-server-alpha.vercel.app',
   headers: {
     'Content-Type': 'application/json',
   },
