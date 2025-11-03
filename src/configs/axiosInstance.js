8a667192a06769c44cf2bfc81a7a1787ca60025b
akshayskurup <akshayskurup@gmail.com>
2025-11-03 23:13:33 +0530

    Update API URL to use HTTPS

--- src/configs/axiosInstance.js
+++ src/configs/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://erp-api.fruition.in.net',
+  baseURL: 'https://erp-api.fruition.in.net',
   headers: {
     'Content-Type': 'application/json',
   }, 
